eedbcf53362af6d2f6899fe1974c1f456d64b128
Felix Yanganaev <yangfell@gmail.com>
2022-03-11 14:31:13 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
-variable "client_id" {}  
-
-variable "client_secret" {}
-
 variable "azure_region" {
   default = "eastus"
 }
@@ -16,35 +12,4 @@ variable "resource_group_name" {
 
 variable "agent_count" {
     default = 2
-}
-
-variable "DB_PASSWORD" {
-  description = "Password for MariaDB admin (get from environment)"
-  type        = string
-}
-
-variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
-}
-
-variable "dns_prefix" {
-  default = "aks01"
-}
-
-variable "cluster_name" {
-  default = "aks01"
-}
-
-variable "log_analytics_workspace_name" {
-  default = "DefaultLogAnalyticsWorkspaceName"
-}
-
-# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
-variable "log_analytics_workspace_location" {
-  default = "eastus"
-}
-
-# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing
-variable "log_analytics_workspace_sku" {
-  default = "PerGB2018"
 }
