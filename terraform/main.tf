b2ebc7a7967ca5ba173f561b0efecc3e97a9f672
Felix Yanganaev <yangfell@gmail.com>
2022-03-11 15:04:26 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,18 @@
 provider "azurerm" {
-  version = "~>2.0"
+  version = "=2.0.0"
   features {}
 }
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "flxdiploma"
-    storage_account_name = "tfstateflxdploma"
-    container_name       = "terraformflxdiploma"
-    key                  = "terraforflxdiploma.tfstate"
+    resource_group_name  = "rg-hello-azure-tf"
+    storage_account_name = "sahelloazuretf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.azure_region
-
-  tags = {
-    Owner = "feliks_ianganaev@epam.com"
-  }
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-hello-azure"
+  location = "northcentralus"
 }
